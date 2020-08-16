6f9a2c2304e66a034ee2ae47923e1da039288f6d
razumovskiy <kolosovp94@gmail.com>
2020-08-16 22:13:16 +0300

    get users chat service implemented

---
+++ Models/UserChats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using ServicesLibrary.Models.Chat;
+
+namespace ServicesLibrary.Models
+{
+    public class UserChats
+    {
+        [JsonProperty("direct-chats")] public List<DirectChat> DirectChats { get; set; }
+        [JsonProperty("channels")] public List<Channel> Channels { get; set; }
+        [JsonProperty("groups")] public List<Group> Groups { get; set; }
+    }
+}
